7606dcf1ff9b902a1f2d37abc5f71037f40fba29
akumImchen <akumimchen1411@gmail.com>
2026-01-06 23:58:31 +0530

    Necessary styling changes

--- src/data/musicMood.js
+++ src/data/musicMood.js
@@ -1,8 +1,8 @@
 const musicByMood={
-    happy:[{image:"./sun.jpg",title:"Here comes the sun",artist:"The Beatles"}],
+    happy:[{image:"./b3.jpg",title:"Here comes the sun",artist:"The Beatles"}],
     sad:[{image:"./powfu.jpg",title:"Dead Bed",artist:"Powfu"}],
     chill:[{image:"./Sunflower.jpg",title:"Sunflower",artist:"Post Malone"}],
-    romantic:[{image:"./data/pool.jpg",title:"The Pool",artist:" Stephen Sanchez"}],
+    romantic:[{image:"./p3.jpg",title:"The Pool",artist:" Stephen Sanchez"}],
 };
 export default musicByMood;
 console.log(musicByMood);
